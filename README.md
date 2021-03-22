c570cce63e4bdae52adb5b5fbd1240e58b7311c4
Dave Goodchild <admin@daves-mbp.home>
2021-03-22 11:53:18 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# RUNNING THE PARSER
+
+* Run ```npm i```.
+* Run ```INPUT=filename npm run clean```.
+* Example: ```INPUT=sample1 npm run clean```.
+
+# INVALID
+
+* strong
+* span
+* small
+* i
+* sub
+* iframe image
+* cite
+* sup
+* h2
+* u
+* br
+* em.
+
+## TRANSFORMS
+
+* HTML figure tags are also not supported. These tags are currently being replaced by the XML media tag defined in the XML schema.
+* HTML blockquote tags now need to be renamed to bq and padded with block tags.
+* p tags only support enriched text format child tags, so media tags are being checked to ensure they are not encapsulated within a p tag.
+* Similarly, p tags cannot sit within a p tag so we first check to see if a p tag is currently open before parsing a new opening p tag.
+* Custom attributes have to be removed in some case
---
+++ index.ts
@@ -0,0 +1,81 @@
+import fs from 'fs';
+import sanitizeHtml from 'sanitize-html';
+
+const allowedTags = [
+  'address',
+  'article',
+  'aside',
+  'footer',
+  'header',
+  'h1',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'hgroup',
+  'main',
+  'nav',
+  'section',
+  'blockquote',
+  'dd',
+  'div',
+  'dl',
+  'dt',
+  'figcaption',
+  'figure',
+  'hr',
+  'li',
+  'main',
+  'ol',
+  'p',
+  'pre',
+  'ul',
+  'a',
+  'abbr',
+  'b',
+  'bdi',
+  'bdo',
+  'code',
+  'data',
+  'dfn',
+  'kbd',
+  'mark',
+  'q',
+  'rb',
+  'rp',
+  'rt',
+  'rtc',
+  'ruby',
+  's',
+  'samp',
+  'time',
+  'var',
+  'wbr',
+  'caption',
+  'col',
+  'colgroup',
+  'table',
+  'tbody',
+  'td',
+  'tfoot',
+  'th',
+  'thead',
+  'tr',
+].sort();
+
+fs.readFile(`./dirty/${process.env.INPUT}.html`, 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  fs.writeFile(
+    `./clean/${process.env.INPUT}.html`,
+    sanitizeHtml(data, {
+      transformTags: {
+        figure: 'h3',
+        blockquote: 'bq',
+      },
+      allowedTags,
+    }),
+    () => {}
+  );
+});
